897888612bd05c10e2cbbd0fd067d12b7b12025e
Artem <footerka2@gmail.com>
2021-07-13 18:29:44 +0300

    Hello World

--- src/main/java/com/example/obuchen/ObuchenApplication.java
+++ src/main/java/com/example/obuchen/ObuchenApplication.java
@@ -2,8 +2,10 @@ package com.example.obuchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ObuchenApplication {
 
     public static void main(String[] args) {
