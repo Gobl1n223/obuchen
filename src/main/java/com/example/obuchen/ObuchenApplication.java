600f76c039be7efcffc68e3db488f0db104fc1c1
Artem <footerka2@gmail.com>
2021-07-13 12:34:29 +0300

    OBUCHENIE

---
+++ src/main/java/com/example/obuchen/ObuchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.obuchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObuchenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObuchenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obuchen/ObuchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obuchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObuchenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
