9af99b02b72a410376f7a3cc48ea0bdc2242c431
Artem <footerka2@gmail.com>
2021-07-13 13:27:21 +0300

    Hello World

--- src/main/java/controllers/HelloWorld.java
+++ src/main/java/controllers/HelloWorld.java
@@ -1,10 +1,11 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class HelloWorld {
-    @RequestMapping("/try")
+    @RequestMapping("/hello")
         public String model(Model model) {
             return "hello";
 
